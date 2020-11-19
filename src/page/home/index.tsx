181cac64d60aae21fbea476a9adb2a395e5df546
wwt <wangwentao@heytea.com>
2020-11-19 16:11:40 +0800

    更新

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,44 +1,26 @@
-import React, {createContext, useEffect} from "react"
-import HD from '../../utils/promise'
-// import {Button} from "../../component/Button/index";
-
-
+import React, { createContext, useEffect } from "react"
 interface IThemeProps {
-    [key: string] : { color: string; background: string}
+  [key: string]: { color: string; background: string }
 }
 
 const themes: IThemeProps = {
-    'light': {
-        color: '#fff',
-        background: '#eee'
-    },
-    'dark': {
-        color: '#000',
-        background: '#222'
-    }
+  'light': {
+    color: '#fff',
+    background: '#eee'
+  },
+  'dark': {
+    color: '#000',
+    background: '#222'
+  }
 }
 
 export const ThemeContext = createContext(themes.light)
 
 export const Main: React.FC = () => {
 
-    useEffect(()=>{
-        const p = new HD((resolve:any, reject:any)=>{
-            resolve('解决')
-            reject('失败')
-        })
-
-        console.log(p);
-        
-
-    })
-
-    return (
-        <>
-            <div>123</div>
-            {/*<ThemeContext.Provider value={themes.light}>*/}
-            {/*    /!*<Button text={'123'}/>*!/*/}
-            {/*</ThemeContext.Provider>*/}
-        </>
-    )
+  return (
+    <>
+      <div>123</div>
+    </>
+  )
 }
